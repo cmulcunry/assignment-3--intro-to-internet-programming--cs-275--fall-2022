e1456a6ee8ec2b96c2c9df269f8d09e6ee0c7708
rrliang <42505429+rrliang@users.noreply.github.com>
2021-10-28 02:05:52 -0400

    Updated app.js
    -Added all required features

---
+++ js/app.js
@@ -0,0 +1,25 @@
+window.onload = () => {
+    const tdList = document.getElementsByTagName(`td`);
+    for (const cell of tdList) {
+        cell.addEventListener(`click`, function() {
+            for (const cells of tdList) {
+                if (cell === cells) {
+                    if (cells.classList.contains(`flipped`)) {
+                        flip(cells);
+                    } else {
+                        cells.classList.remove(`initial`);
+                        cells.classList.add(`flipped`);
+                    }
+                } else if (cells.classList.contains(`flipped`)) {
+                    flip(cells);
+                } else {
+                    cells.classList.add(`initial`);
+                }
+            }
+        });
+    }
+    let flip = (cell) => {
+        cell.classList.remove(`flipped`);
+        cell.classList.add(`initial`);
+    };
+};
