d82517c8d75c4ba82123510abdb9296ead9298dd
Scottk123 <skennedy@hartford.edu>
2021-10-31 22:05:23 -0400

    Added the gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,166 @@
+const { src, dest, series, watch } = require(`gulp`);
+const htmlValidator = require(`gulp-html`);
+const htmlCompressor = require(`gulp-htmlmin`);
+const jsLinter = require(`gulp-eslint`);
+const jsCompressor = require(`gulp-uglify`);
+const sass = require(`gulp-sass`)(require(`sass`));
+const babel = require(`gulp-babel`);
+const browserSync = require(`browser-sync`);
+const reload = browserSync.reload;
+
+let browserChoice = `default`;
+
+async function brave () {
+    browserChoice = `brave browser`;
+}
+
+async function chrome () {
+    browserChoice = `google chrome`;
+}
+
+async function edge () {
+    // In Windows, the value might need to be “microsoft-edge”. Note the dash.
+    browserChoice = `microsoft edge`;
+}
+
+async function firefox () {
+    browserChoice = `firefox`;
+}
+
+async function opera () {
+    browserChoice = `opera`;
+}
+
+async function safari () {
+    browserChoice = `safari`;
+}
+
+async function vivaldi () {
+    browserChoice = `vivaldi`;
+}
+
+async function allBrowsers () {
+    browserChoice = [
+        `brave browser`,
+        `google chrome`,
+        `microsoft edge`, // Note: In Windows, this might need to be microsoft-edge
+        `firefox`,
+        `opera`,
+        `safari`,
+        `vivaldi`
+    ];
+}
+
+let validateHTML = () => {
+    return src([`html/*.html`, `html/**/*.html`]).pipe(htmlValidator());
+};
+
+let compressHTML = () => {
+    return src([`html/*.html`,`html/**/*.html`])
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
+
+let compileCSSForDev = () => {
+    return src(`css/style.css`)
+        .pipe(sass.sync({
+            outputStyle: `expanded`,
+            precision: 10
+        }).on(`error`, sass.logError))
+        .pipe(dest(`temp/css`));
+};
+
+let lintJS = () => {
+    return src(`js/*.js`)
+        .pipe(jsLinter())
+        .pipe(jsLinter.formatEach(`compact`));
+};
+
+let compileCSSForProd = () => {
+    return src(`css/style.css`)
+        .pipe(sass.sync({
+            outputStyle: `compressed`,
+            precision: 10
+        }).on(`error`, sass.logError))
+        .pipe(dest(`prod/css`));
+};
+
+let transpileJSForDev = () => {
+    return src(`js/*.js`)
+        .pipe(babel())
+        .pipe(dest(`temp/js`));
+};
+
+let transpileJSForProd = () => {
+    return src(`js/*.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/js`));
+};
+
+let copyUnprocessedAssetsForProd = () => {
+    return src([
+        `css/*.*`,
+        `js/*.*`,
+        `!html/`,    // but not the HTML folder
+        `!html/*.*`, // or any files in it
+        `!html/**`,  // or any sub folders;
+        `!js/*.js`,  // ignore JS;
+        `!css/**` // and, ignore Sass/CSS.
+    ], {dot: true})
+        .pipe(dest(`prod`));
+};
+
+let serve = () => {
+    browserSync({
+        notify: true,
+        reloadDelay: 50,
+        browser: browserChoice,
+        server: {
+            baseDir: [
+                `temp`,
+                `html`
+            ]
+        }
+    });
+
+    watch(`js/*.js`, series(lintJS, transpileJSForDev))
+        .on(`change`, reload);
+
+    watch(`css/style.css`, compileCSSForDev)
+        .on(`change`, reload);
+
+    watch(`html/*.html`, validateHTML)
+        .on(`change`, reload);
+};
+
+exports.brave = series(brave, serve);
+exports.chrome = series(chrome, serve);
+exports.edge = series(edge, serve);
+exports.firefox = series(firefox, serve);
+exports.opera = series(opera, serve);
+exports.safari = series(safari, serve);
+exports.vivaldi = series(vivaldi, serve);
+exports.allBrowsers = series(allBrowsers, serve);
+exports.validateHTML = validateHTML;
+exports.compressHTML = compressHTML;
+exports.HTMLProcessing = series(validateHTML, compressHTML);
+exports.lintJS = lintJS;
+exports.compileCSSForDev = compileCSSForDev;
+exports.transpileJSForDev = transpileJSForDev;
+exports.compileCSSForProd = compileCSSForProd;
+exports.transpileJSForProd = transpileJSForProd;
+exports.copyUnprocessedAssetsForProd = copyUnprocessedAssetsForProd;
+exports.serve = series(
+    validateHTML,
+    compileCSSForDev,
+    lintJS,
+    transpileJSForDev,
+    serve
+);
+exports.build = series(
+    compressHTML,
+    compileCSSForProd,
+    transpileJSForProd,
+    copyUnprocessedAssetsForProd
+);
