2c72742885baaa7bab259b3f6d9cfa3a17d487c9
Scottk123 <skennedy@hartford.edu>
2021-11-05 22:08:28 -0400

    The letters return to idle when clicked again

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 let letters = document.querySelectorAll(`td`);
+let flippedLetter;
 
 letters.forEach(letter => {
     letter.addEventListener(`click`, () => {
-        letter.style.transform = `rotate(180deg)`;
-        letter.style.color = `blue`;
+        if(flippedLetter !== letter) {
+            letter.style.transform = `rotate(180deg)`;
+            letter.style.color = `blue`;
+            flippedLetter = letter;
+        }
+        else {
+            letter.style.transform = `rotate(0deg)`;
+            letter.style.color = `black`;
+            flippedLetter = null;
+        }
     });
 });
