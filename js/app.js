3eb18a45308fbd1e79b2ce5bc0d1295a3d1643ec
Scottk123 <skennedy@hartford.edu>
2021-11-05 23:06:30 -0400

    The letters now take 0.5 sec to rotate

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 let letters = document.querySelectorAll(`td`);
 let flippedLetter;
+let rotationTimer;
+let angle = 0;
 
 letters.forEach(letter => {
     letter.addEventListener(`click`, () => {
         if(flippedLetter !== letter) {
-            letter.style.transform = `rotate(180deg)`;
+            //letter.style.transform = `rotate(180deg)`;
             letter.style.color = `blue`;
             flippedLetter = letter;
+            rotationTimer = setInterval(rotate, 3);
         }
         else {
             letter.style.transform = `rotate(0deg)`;
@@ -15,3 +18,10 @@ letters.forEach(letter => {
         }
     });
 });
+
+function rotate() {
+    angle++;
+    flippedLetter.style.transform = `rotate(${angle}deg)`;
+    if(180 === angle)
+        clearInterval(rotationTimer);
+}
