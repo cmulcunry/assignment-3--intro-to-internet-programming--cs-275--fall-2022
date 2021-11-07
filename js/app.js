5cd65cf0274ec8454fa5a25e1e20a6f1f99e2111
mikeshank8800 <89604251+mikeshank8800@users.noreply.github.com>
2021-11-07 13:48:09 -0500

    Michael Sciancalepore Assignment 3 Completed

---
+++ js/app.js
@@ -0,0 +1,42 @@
+let previousLetter = null;
+
+function rotate(element) {
+    while(previousLetter == null) {
+        element.classList.remove(`flipped`);
+        element.classList.add(`normal`);
+        previousLetter = element;
+    }
+    if(element === previousLetter) {
+        if(previousLetter.classList.contains(`flipped`)) {
+            previousLetter.classList.remove(`flipped`);
+            previousLetter.classList.add(`normal`);
+        }
+        else {
+            previousLetter.classList.remove(`normal`);
+            previousLetter.classList.add(`flipped`);
+        }
+        previousLetter = element;
+    }
+    else {
+        if(!element.classList.contains(`flipped`)) {
+            element.classList.remove(`normal`);
+            element.classList.add(`flipped`);
+        }
+        else if (element.classList.contains(`flipped`)) {
+            element.classList.remove(`flipped`);
+            element.classList.add(`normal`);
+        }
+        if(previousLetter.classList.contains(`flipped`)) {
+            previousLetter.classList.remove(`flipped`);
+            previousLetter.classList.add(`normal`);
+        }
+        previousLetter = element;
+    }
+}
+
+window.onclick = element => {
+    let letterClicked = element.target;
+    if(letterClicked.tagName == `TD`) {
+        rotate(letterClicked);
+    }
+};
