94c593a861e7d3f9beb942cd58348c3560e8db22
Scottk123 <skennedy@hartford.edu>
2021-11-07 18:49:45 -0500

    Fixed bug where cells wouldn't rotate back when click again

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let letters = document.querySelectorAll(`td`);
-let flippedLetter;
-let tempLetter;
+let flippedLetter = 0;
+let tempLetter = 0;
 let rotationTimer;
 let angle = 0;
 
@@ -8,26 +8,33 @@ letters.forEach(letter => {
     letter.addEventListener(`click`, () => {
         angle = 0;
         clearInterval(rotationTimer);
-        tempLetter = flippedLetter;
         rotationTimer = setInterval(rotate, 3);
         if(flippedLetter !== letter) {
             //letter.style.transform = `rotate(180deg)`;
+            tempLetter = flippedLetter;
             letter.style.color = `blue`;
             flippedLetter = letter;
         }
         else {
             //letter.style.transform = `rotate(0deg)`;
             //letter.style.color = `black`;
-            flippedLetter = null;
+            tempLetter = letter;
+            flippedLetter = 0;
         }
+        tempLetter.style.color = `black`;
     });
 });
 
 function rotate() {
-    angle++;
-    flippedLetter.style.transform = `rotate(${angle}deg)`;
-    tempLetter.style.transform = `rotate(${180 - angle}deg)`;
-    tempLetter.style.color = `black`;
-    if(180 === angle)
+    if(180 === angle){
+        angle = 0;
         clearInterval(rotationTimer);
+    }
+    else {
+        angle++;
+        if(flippedLetter)
+            flippedLetter.style.transform = `rotate(${angle}deg)`;
+        if(tempLetter)
+            tempLetter.style.transform = `rotate(${180 - angle}deg)`;
+    }
 }
