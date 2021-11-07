a8bf30bf35c3a9b0d4e7cfacb0a13e3a4dbba0ba
cmulcunry <89563322+cmulcunry@users.noreply.github.com>
2021-11-07 18:21:09 -0500

    start of javascript using constants, doesn't do anything as of yet

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 window.onload = () => {
+const table = document.getElementsByTagName('td');
 
+let flip = (letter) => {
+    letter.classList.remove(`flipped`);
+    letter.classList.add('default');
+    }
+
+for (const letter of table) {
+    letter.addEventListener(`click`, flip);
+    }
 }
