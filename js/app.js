bd030bbccc5c29e0bac297afdef37863a9e82858
cmulcunry <89563322+cmulcunry@users.noreply.github.com>
2021-11-07 20:03:49 -0500

    adjusted the logic a bit, removed function and eventlistener

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 window.onload = () => {
-const table = document.getElementsByTagName('td');
+    const table = document.getElementsByTagName('td');
 
-let flip = (letter) => {
-    letter.classList.remove(`flipped`);
-    letter.classList.add('default');
-    }
-
-for (const letter of table) {
-    letter.addEventListener(`click`, flip);
+    for (const letter of table) {
+        for (const letters of table) {
+            if (letter === letters) {
+                if (letters.classList.contains(`flipped`)) {
+                    letter.classList.remove(`default`);
+                    letter.classList.add(`flipped`);
+                }
+                else {
+                    letter.classList.remove(`flipped`);
+                    letter.classList.add(`default`);
+                }
+            }
+        }
     }
 }
