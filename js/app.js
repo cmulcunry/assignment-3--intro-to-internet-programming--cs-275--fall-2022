bd66edb103af9e1cf5750ba1b953a1410aed479f
cmulcunry <89563322+cmulcunry@users.noreply.github.com>
2021-11-07 22:24:38 -0500

    fixed css

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 window.onload = () => {
+    //Decided to use classlist to determine the state of the letters. Info from here:
+    //https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
     const table = document.getElementsByTagName('td');
 
     for (const letter of table) {
