1c17f2027a70b09b049e843ab21e1f0f98d57031
cmulcunry <89563322+cmulcunry@users.noreply.github.com>
2021-11-08 00:40:11 -0500

    js linting

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
 window.onload = () => {
     //Decided to use classlist to determine the state of the letters. Info from here:
     //https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
-    const table = document.getElementsByTagName('td');
+    const table = document.getElementsByTagName(`td`);
 
     for (const letter of table) {
         letter.addEventListener(`click`, function() {
-        for (const letters of table) {
-            if (letter === letters) {
-                if (letters.classList.contains(`flipped`)) {
+            for (const letters of table) {
+                if (letter === letters) {
+                    if (letters.classList.contains(`flipped`)) {
+                        letters.classList.remove(`flipped`);
+                        letters.classList.add(`default`);
+                    }
+                    else {
+                        letters.classList.remove(`default`);
+                        letters.classList.add(`flipped`);
+                    }
+                }
+                else if (letters.classList.contains(`flipped`)){
                     letters.classList.remove(`flipped`);
                     letters.classList.add(`default`);
                 }
-                else {
-                    letters.classList.remove(`default`);
-                    letters.classList.add(`flipped`);
-                }
             }
-            else if (letters.classList.contains(`flipped`)){
-                letters.classList.remove(`flipped`);
-                letters.classList.add(`default`);
-            }
-        }
         });
     }
 };
