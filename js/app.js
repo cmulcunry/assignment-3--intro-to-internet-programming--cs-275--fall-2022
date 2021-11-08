88553fb73742c1123d922fb8c099b497148c31d5
BorjaErika <78446815+BorjaErika@users.noreply.github.com>
2021-11-08 11:48:56 -0500

    update app.js

--- js/app.js
+++ js/app.js
@@ -2,27 +2,27 @@ let letter =null;
 
 function rotate(element){
     while(letter==null){
-        element.classList.remove('spin');
+        element.classList.remove( `spin`);
         letter=element;
     }
     if(element ===letter){
-        if(letter.classList.contains('spin')){
-            letter.classList.remove('spin');
+        if(letter.classList.contains( `spin`)){
+            letter.classList.remove(`spin`);
         }
         else{
-            letter.classList.add('flipped');
+            letter.classList.add(`spin`);
         }
         letter=element;
     }
     else{
-        if(!element.classList.contains('spin')){
+        if(!element.classList.contains(`spin`)){
             element.classList.add(`spin`);
         }
-        else if (element.classList.contains('spin')){
-            element.classList.remove('spin');
+        else if (element.classList.contains(`spin`)){
+            element.classList.remove(`spin`);
         }
-        if (letter.classList.contains('spin')){
-            letter.classList.remove('spin');
+        if (letter.classList.contains(`spin`)){
+            letter.classList.remove(`spin`);
         }
         letter=element;
     }
