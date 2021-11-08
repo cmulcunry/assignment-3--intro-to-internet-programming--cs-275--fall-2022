db98c1197da0a7c319032fafdf11fb1024d6f029
cmulcunry <89563322+cmulcunry@users.noreply.github.com>
2021-11-07 22:20:55 -0500

    fixed event listener, letters will turn but not reset other letters

--- js/app.js
+++ js/app.js
@@ -2,17 +2,19 @@ window.onload = () => {
     const table = document.getElementsByTagName('td');
 
     for (const letter of table) {
+        letter.addEventListener(`click`, function() {
         for (const letters of table) {
             if (letter === letters) {
                 if (letters.classList.contains(`flipped`)) {
-                    letter.classList.remove(`default`);
-                    letter.classList.add(`flipped`);
-                }
-                else {
                     letter.classList.remove(`flipped`);
                     letter.classList.add(`default`);
                 }
+                else {
+                    letter.classList.remove(`default`);
+                    letter.classList.add(`flipped`);
+                }
             }
         }
+     });
     }
 }
