e77191866e122615865b17b9e4a6b7fcb93dd425
Jared Kozar <jared.rkozar@gmail.com>
2021-11-08 21:54:36 -0500

    sortened the app.js code

--- js/app.js
+++ js/app.js
@@ -1,88 +1,52 @@
+var tableCellA = document.querySelectorAll(`td`)[0];
+var tableCellB = document.querySelectorAll(`td`)[1];
+var tableCellC = document.querySelectorAll(`td`)[2];
+var tableCellD = document.querySelectorAll(`td`)[3];
+var cells = [tableCellA, tableCellB, tableCellC, tableCellD];
+var body = document.body;
+var style = getComputedStyle(document.body);
+
 window.onload = () => {
-    const body = document.body;
-    var style = getComputedStyle(document.body);
-    let tableCellA = document.querySelectorAll(`td`)[0];
-    let tableCellB = document.querySelectorAll(`td`)[1];
-    let tableCellC = document.querySelectorAll(`td`)[2];
-    let tableCellD = document.querySelectorAll(`td`)[3];
+    let resetButton = document.getElementById("reset-bg");
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener(`click`, flipCell, false);
+    }
+    resetButton.addEventListener(`click`, reset, false);
+};
 
-    let cellAFlipped = false;
-    let cellBFlipped = false;
-    let cellCFlipped = false;
-    let cellDFlipped = false;
+function flipCell() {
+    for (let i = 0; i < cells.length; i++) { 
+        if (this.innerHTML == cells[i].innerHTML) {
+            cells[i].style.transition = `transform .5s`;
+            cells[i].style.transform = `rotate(180deg)`;
 
-    let cells = [tableCellA, tableCellB, tableCellC, tableCellD];
-
-    let flipCellA = () => {
-        if (cellAFlipped == true) {
-            reset();
+            if (i == 0) {
+                body.style.background = style.getPropertyValue(`--cellAColor`);
+            } else if (i == 1) {
+                body.style.background = style.getPropertyValue(`--cellBColor`);
+            } else if (i == 2) {
+                body.style.background = style.getPropertyValue(`--cellCColor`);
+            } else if (i == 3) {
+                body.style.background = style.getPropertyValue(`--cellDColor`);
+            }
         } else {
-            reset();
-            cellAFlipped = true;
-            tableCellA.style.transition = `transform .5s`;
-            tableCellA.style.transform = `rotate(180deg)`;
-        }
-
-        body.style.background = style.getPropertyValue(`--cellAColor`);
-    };
-
-    let flipCellB = () => {
-        if (cellBFlipped == true) {
-            reset();
-        } else {
-            reset();
-            cellBFlipped = true;
-            tableCellB.style.transition = `transform .5s`;
-            tableCellB.style.transform = `rotate(180deg)`;
-        }
-
-        body.style.background = style.getPropertyValue(`--cellBColor`);
-    };
-
-    let flipCellC = () => {
-        if (cellCFlipped == true) {
-            reset();
-        } else {
-            reset();
-            cellCFlipped = true;
-            tableCellC.style.transition = `transform .5s`;
-            tableCellC.style.transform = `rotate(180deg)`;
-        }
-
-        body.style.background = style.getPropertyValue(`--cellCColor`);
-    };
-
-    let flipCellD = () => {
-        if (cellDFlipped == true) {
-            reset();
-        } else {
-            reset();
-            cellDFlipped = true;
-            tableCellD.style.transition = `transform .5s`;
-            tableCellD.style.transform = `rotate(180deg)`;
-        }
-
-        body.style.background = style.getPropertyValue(`--cellDColor`);
-    };
-
-    function reset() {
-        for (let i = 0; i < cells.length; i++) { 
             cells[i].style.transition = `transform .5s`;
             cells[i].style.transform = `rotate(0deg)`;
         }
-        cellAFlipped = false;
-        cellBFlipped = false;
-        cellCFlipped = false;
-        cellDFlipped = false;
     }
-    
-    document.getElementById(`reset-bg`).onclick = () => {
-        body.style.background = style.getPropertyValue(`--bgcolor`);
-        reset();
-    };
+}
 
-    tableCellA.addEventListener(`click`, flipCellA);
-    tableCellB.addEventListener(`click`, flipCellB);
-    tableCellC.addEventListener(`click`, flipCellC);
-    tableCellD.addEventListener(`click`, flipCellD);
+function reset() {
+    for (let i = 0; i < cells.length; i++) { 
+        cells[i].style.transition = `transform .5s`;
+        cells[i].style.transform = `rotate(0deg)`;
+    }
+
+    body.style.background = style.getPropertyValue(`--bgcolor`);
+}
+
+document.getElementById(`reset-bg`).onclick = () => {
+    var style = getComputedStyle(document.body);
+    body.style.background = style.getPropertyValue(`--bgcolor`);
+    reset();
 };
