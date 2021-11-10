85906c34a42cd1305e984b1f4feb5c891a341f48
DavidJurich <davjur@gmail.com>
2021-11-10 18:29:00 -0500

    Update App.js
    Work on either numerous functions to switch between states of animation or make one comprehensive function to factor if statements for rules for states of animation.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,19 @@ window.onload = () => {
     function animation() {
         this.classList.toggle(`animate`);
     }
+
+    function restore() {
+        this.classList.toggle(`origin`);
+    }
+
     letter.forEach((letter) => letter.addEventListener(`click`, animation));
+
+    // function state() {
+    //     letter.forEach((letter) => letter.addEventListener(`click`, animation));
+    //     letter.forEach((letter) => letter.addEventListener(`click`, restore));
+
+
+    // }
 
 };
 
