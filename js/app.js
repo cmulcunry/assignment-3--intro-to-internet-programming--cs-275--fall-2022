3700339906d8af3b3a670ebfbf22e4a7c88a7dff
DavidJurich <davjur@gmail.com>
2021-11-10 18:41:25 -0500

    Work on JS functionality
    Decided one comprehensive function is the best approach, work on iterating through the given list, replace and add animation states. Need to create more variables.

--- js/app.js
+++ js/app.js
@@ -2,12 +2,45 @@ window.onload = () => {
     const letter = document.querySelectorAll(`td`);
 
     function animation() {
-        this.classList.toggle(`animate`);
+        if (letter[0].classList.contains(`animation`)) {
+            letter[0].classList.add(`animate`);
+            letter[1].classList.add(`origin`);
+            letter[2].classList.add(`origin`);
+            letter[3].classList.add(`origin`);
+        }
+        if (letter[1].classList.contains(`animation`)) {
+            this.classList.toggle(`animate`);
+            letter[0].classList.add(`origin`);
+            letter[2].classList.add(`origin`);
+            letter[3].classList.add(`origin`);
+        }
+
+        if (letter[2].classList.contains(`animation`)) {
+            this.classList.toggle(`animate`);
+            letter[0].classList.add(`origin`);
+            letter[1].classList.add(`origin`);
+            letter[3].classList.add(`origin`);
+        }
+
+        if (letter[3].classList.contains(`animation`)) {
+            this.classList.toggle(`animate`);
+            letter[0].classList.add(`origin`);
+            letter[1].classList.add(`origin`);
+            letter[2].classList.add(`origin`);
+        }
+        // this.classList.toggle(`animate`);
     }
 
-    function restore() {
-        this.classList.toggle(`origin`);
-    }
+    // function restore() {
+    //     this.classList.toggle(`origin`);
+    // }
+
+    // if (letter.classList.contains(`animation`)) 
+    // {
+    //     letter.restore();
+
+
+    // }
 
     letter.forEach((letter) => letter.addEventListener(`click`, animation));
 
