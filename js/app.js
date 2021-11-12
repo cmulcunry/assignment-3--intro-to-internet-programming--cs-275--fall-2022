f9afb32f0ae02d8bde6ce19ab4b893d619ecdb26
DavidJurich <davjur@gmail.com>
2021-11-11 20:59:33 -0500

    Finish assignment
    Update style.css to fully comply with lintCSS, Update app.js to remove previous even/odd statement. Updated function with if else statements to check for animation states.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,18 @@
-window.onload = () => {
-    let count = -1;
-    const table = document.getElementsByTagName(`td`);
-
-
-    function onClick(){
-        count += 1;
-
-        if(count%2 == 0){
-            table.classList.add(`animate`);
-        } else {
-            table.classList.add(`origin`);
-        }
+const tables = document.getElementsByTagName(`td`);
+function animate() {
+    for (const index of tables) {
+        index.addEventListener(`click`, () => {
+            for (const letter of tables) {
+                if (index === letter) {
+                    if (letter.classList.contains(`animate`))
+                        letter.classList.add(`origin`);
+                    else
+                        letter.classList.add(`animate`);
+                } else if (letter.classList.contains(`animate`))
+                    letter.classList.add(`origin`);
+            }
+        });
     }
-    window.onclick = element => {
-        let letter = element.target;
-        if(letter.tagName === `td`) {
-            onClick(letter);
-        }
-    };
-};
+}
+
+window.onload = animate();
