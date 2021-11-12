14bd0179e52af7d41c7b1aa7a30168f99e184d0d
Derekdel989 <78804445+Derekdel989@users.noreply.github.com>
2021-11-11 21:15:40 -0500

    Starting 1

---
+++ js/app.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+    const TableCell = document.querySelectorAll(`td`);
+    TableCell[0].addEventListener(`click`,() => {
+        TableCell[0].classList.add(`rotate`);
+    })
+};
