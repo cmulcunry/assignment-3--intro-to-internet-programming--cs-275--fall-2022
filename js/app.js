1b1cb0830f307d889f59180b886d942113597560
DavidJurich <davjur@gmail.com>
2021-11-11 19:33:25 -0500

    Update app.js
    Update with a new function to add/remove animation on odd/even clicks on table.

--- js/app.js
+++ js/app.js
@@ -1,59 +1,21 @@
 window.onload = () => {
-    const letter = document.querySelectorAll(`td`);
-    let pointer;
+    let count = -1;
+    const table = document.getElementsByTagName(`td`);
 
-    function animation() {
-        if (letter[0].classList.contains(`animation`)) {
-            letter[0].classList.add(`animate`);
-            letter[1].classList.add(`origin`);
-            letter[2].classList.add(`origin`);
-            letter[3].classList.add(`origin`);
-        }
-        if (letter[1].classList.contains(`animation`)) {
-            this.classList.toggle(`animate`);
-            letter[0].classList.add(`origin`);
-            letter[2].classList.add(`origin`);
-            letter[3].classList.add(`origin`);
-        }
 
-        if (letter[2].classList.contains(`animation`)) {
-            this.classList.toggle(`animate`);
-            letter[0].classList.add(`origin`);
-            letter[1].classList.add(`origin`);
-            letter[3].classList.add(`origin`);
-        }
+    function onClick(){
+        count += 1;
 
-        if (letter[3].classList.contains(`animation`)) {
-            this.classList.toggle(`animate`);
-            letter[0].classList.add(`origin`);
-            letter[1].classList.add(`origin`);
-            letter[2].classList.add(`origin`);
+        if(count%2 == 0){
+            table.classList.add(`animate`);
+        } else {
+            table.classList.add(`origin`);
         }
-        // this.classList.toggle(`animate`);
     }
-
-    // function restore() {
-    //     this.classList.toggle(`origin`);
-    // }
-
-    // if (letter.classList.contains(`animation`)) 
-    // {
-    //     letter.restore();
-
-
-    // }
-
-    letter.forEach((letter) => letter.addEventListener(`click`, animation));
-
-    // function state() {
-    //     letter.forEach((letter) => letter.addEventListener(`click`, animation));
-    //     letter.forEach((letter) => letter.addEventListener(`click`, restore));
-
-
-    // }
-
+    window.onclick = element => {
+        let letter = element.target;
+        if(letter.tagName === `td`) {
+            onClick(letter);
+        }
+    };
 };
-
-// // setTimeout(function() {
-// //     letters.classList.add(`origin`);
-// // }
